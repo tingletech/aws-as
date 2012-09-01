b148384c77fe0a522df3a0cd2811b6c54b5beab7
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-08-31 19:49:53 -0700

    based on testing; but still untested... won't work yet

--- launch-ec2.sh
+++ launch-ec2.sh
@@ -14,7 +14,6 @@ hackconf() {	# poor man's templates; hard coded for %{DB_URL} and %{password}
 password=`cat ~/.ec2/.dbpass`
 # get the hostname for the database
 endpoint=`rds-describe-db-instances $DB_INSTANCE_IDENTIFIER | head -1 | awk '{ print $9 }'`
-endpoint="endpoint"
 
 db_url="jdbc:mysql://$endpoint:3306/archivesspace?user=aspace\&password=$password"
 #                                                            ^ escaped for regex ...
@@ -32,17 +31,36 @@ set -eux
 # this gets run as root on the amazon machine when it boots up
 
 # install packages we need from amazon's repo
+yum -y update			# get the latest security updates
 yum -y install git tomcat7
-yum -y install mysql-bench
+yum -y install ant 		# ant via ./build was crying about tools.jar b/c it was running in JRE
+yum -y install mysql-bench	# assuming this is needed for mysql client??
+# yum -y install monit		# set this up later
+yum -y install tree
+
+# iotop is a handy utility on linux
+easy_install pip
+pip install http://guichaz.free.fr/iotop/files/iotop-0.4.4.tar.gz
+
+# node speeds up sprockets asset pipeline generation during build
+# https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
+yum -y localinstall --nogpgcheck http://nodejs.tchol.org/repocfg/amzn1/nodejs-stable-release.noarch.rpm 
+yum -y install nodejs-compat-symlinks npm
+
+yum -y install cpan  # would be cool to get ack working
 
 # create role account for the application
 useradd aspace
+# so we can read the log files
+gpasswd -a ec2-user tomcat
+gpasswd -a aspace tomcat
 
 # execute this script as the role account
 cat > ~aspace/init.sh <<EOSETUP
 DELIM
 
 # middle of payload user-data file
+
 
 # as_role_account.sh will be run as the role account on the AWS EC2 server
 # hack sensitive info into the script
@@ -53,14 +71,50 @@ cat as_role_account.sh >> aws_init.sh
 # finish off the user-data payload file
 cat >> aws_init.sh << DELIM
 EOSETUP
-# still on remote machine
+# back on remote machine as root
 su - -c aspace ~aspace/init.sh
 
 # tweak environment
-# java -DARCHIVESSPACE_BACKEND=localhost:8089
+# java -DARCHIVESSPACE_BACKEND=localhost:8089 ??
 
+
+# need two tomcats (one for frontend, one for backend)
+# from /etc/sysconfig/tomcat7 on Amazon Linux AMI:
+# To change values for a specific service make your edits here.
+# To create a new service create a link from /etc/init.d/<your new service> to
+# /etc/init.d/tomcat7 (do not copy the init script) 
+ln -s /etc/init.d/tomcat7 /etc/init.d/tomcat7back
+
+# and make a copy of the
+# /etc/sysconfig/tomcat7 file to /etc/sysconfig/<your new service>
+cp /etc/sysconfig/tomcat7 /etc/sysconfig/tomcat7back
+cp -rp /usr/share/tomcat7/ /usr/share/tomcat7back
+
+# http://serverfault.com/a/252042
+# iptables -t nat -I PREROUTING -p tcp -i eth0 --dport 8080 -j DNAT --to-destination 127.0.0.1:80 -- did not work :(
+# http://www.klawitter.de/tomcat80.html
+# http://forum.slicehost.com/index.php?p=/discussion/2497/iptables-redirect-port-80-to-port-8080/p1
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+
+# public_address=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+
+# and change
+# the property values so the two services won't conflict.
+## sed sed sed
+
+# Register the new
+# service in the system as usual (see chkconfig and similars).
+## chkconfig!
 # install war files into tomcat
-# actually... need two tomcats...
+cp /home/aspace/archivesspace/frontend/frontend.war /usr/share/tomcat7/webapps/ROOT.war
+cp /home/aspace/archivesspace/backend/backend.war /usr/share/tomcat7back/webapps/ROOT.war
+service tomcat7 start
+
+
+# http://www.excelsior-usa.com/articles/tomcat-amazon-ec2-basic.html
+# To have Tomcat start automatically on instance boot, issue the following commands:
+chkconfig --level 345 tomcat7 on
+chkconfig --level 345 tomcat7back on
 
 # notifications?
 DELIM

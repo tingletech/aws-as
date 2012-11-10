9e81cbf36e015888f5e1f3ca3a6d92d0e206ca98
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-11-10 13:34:29 -0800

    need "build" to be able to dbmigrate mysql

--- upload_files.py
+++ upload_files.py
@@ -13,9 +13,11 @@ bw = 'backend.' + tag + '.war'
 fw = 'frontend.' + tag + '.war'
 fn_public = 'public-files/'
 fn_private = 'private-files/'
+build = 'public-files/as-build.' + tag + '.zip'
 url_upload_public = c.generate_url(3600, 'PUT', 'archivesspace', fn_public + fn, headers={'x-amz-acl': 'public-read'})
 url_upload_public_back = c.generate_url(3600, 'PUT', 'archivesspace', fn_public + bw, headers={'x-amz-acl': 'public-read'})
 url_upload_public_front = c.generate_url(3600, 'PUT', 'archivesspace', fn_public + fw, headers={'x-amz-acl': 'public-read'})
+url_upload_public_build = c.generate_url(3600, 'PUT', 'archivesspace', build , headers={'x-amz-acl': 'public-read'})
 
 url_upload_private = c.generate_url(3600, 'PUT', 'archivesspace', fn_private + fn )
 url_upload_private_back = c.generate_url(3600, 'PUT', 'archivesspace', fn_private + bw )
@@ -30,4 +32,5 @@ print 'zip backend/backend.war -d "*mysql-connector*"'
 print 'curl --request PUT --upload-file archivesspace.jar -H \'x-amz-acl: public-read\' "' + url_upload_public + '"'
 print 'curl --request PUT --upload-file backend/backend.war -H \'x-amz-acl: public-read\' "' + url_upload_public_back + '"'
 print 'curl --request PUT --upload-file frontend/frontend.war -H \'x-amz-acl: public-read\' "' + url_upload_public_front + '"'
+print 'curl --request PUT --upload-file build.zip -H \'x-amz-acl: public-read\' "' +  url_upload_public_build + '"'
 

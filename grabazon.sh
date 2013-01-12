9bb477a15d88cf09c7c269eaf6f51746fbbfd0df
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-01-12 03:23:06 +0000

    this is for the old Amazon command line tools

--- grabazon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-# just run this once; grabs the AWS command line tools for EC2 and RDS
-set -eu
-which wget unzip
-wget http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip 
-unzip ec2-api-tools.zip
-wget http://s3.amazonaws.com/rds-downloads/RDSCli.zip 
-unzip RDSCli.zip

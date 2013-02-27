00c0113c67dc7078fd80c28ded718e73776dcf2d
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-02-27 21:16:43 +0000

    fine-tune launch tweeks, should be ready for 0.4.0

--- launch-ec2.sh
+++ launch-ec2.sh
@@ -16,31 +16,41 @@ password=`cat ~/.ec2/.dbpass`
 # see if the database is up
 endpoint=`aws --region $EC2_REGION rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER | jq .DBInstances[0].Endpoint.Address -r`
 echo $endpoint
-if [ "$endpoint" != 'null' ]; then
-  echo "$endpoint" is already running
-  exit 1
+if [ "$endpoint" != 'null' ]
+  then
+    echo "$endpoint" is already running
+    echo "Do you start an EC2 still?"
+    # http://stackoverflow.com/a/226724/1763984
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) break;;
+            No ) exit;;
+        esac
+    done
+  else
+    # launch the database
+    # http://docs.amazonwebservices.com/AmazonRDS/latest/CommandLineReference/CLIReference-cmd-CreateDBInstance.html
+    #rds-create-db-instance            \
+    aws --region $EC2_REGION rds create-db-instance \
+      --db-instance-identifier $DB_INSTANCE_IDENTIFIER \
+      --db-instance-class $RDS_SIZE \
+      --db-parameter-group-name utf8  \
+      --engine MySQL                  \
+      --db-name archivesspace         \
+      --master-user-password $password  \
+      --port 3306                     \
+      --backup-retention-period 1     \
+      --allocated-storage 10          \
+      --master-username aspace        \
+      --publicly-accessible           \
+      --availability-zone $ZONE
 fi
-
-# launch the database
-# http://docs.amazonwebservices.com/AmazonRDS/latest/CommandLineReference/CLIReference-cmd-CreateDBInstance.html
-#rds-create-db-instance            \
-aws --region $EC2_REGION rds create-db-instance \
-  --db-instance-identifier $DB_INSTANCE_IDENTIFIER \
-  --db-instance-class $RDS_SIZE \
-  --db-parameter-group-name utf8  \
-  --engine MySQL                  \
-  --db-name archivesspace         \
-  --master-user-password $password  \
-  --port 3306                     \
-  --backup-retention-period 1     \
-  --allocated-storage 10          \
-  --master-username aspace        \
-  --availability-zone $ZONE
 
 
 while [ "$endpoint" == 'null' ]
   do
-  sleep 2
+  sleep 15 
+  echo "."
   endpoint=`aws --region $EC2_REGION rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER | jq .DBInstances[0].Endpoint.Address -r`
   done
 
@@ -67,6 +77,9 @@ yum -y install ant
 yum -y install http://fr2.rpmfind.net/linux/dag/redhat/el5/en/x86_64/dag/RPMS/daemonize-1.6.0-1.el5.rf.x86_64.rpm
 # yum -y install ftp://rpmfind.net/linux/dag/redhat/el5/en/i386/dag/RPMS/daemonize-1.6.0-1.el5.rf.i386.rpm
 
+# twincat tomcat setup needs xsltproc
+yum install -y libxslt
+
 # these aren't strictly nessicary for the application but will be usful for debugging
 
 # iotop is a handy utility on linux
@@ -86,7 +99,7 @@ iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 80
 DELIM
 
 # only on the t1.micro, tune swap and turn off sendmail
-if [ "$EC2_SIZE" == 't1.micro']; then
+if [ "$EC2_SIZE" == 't1.micro' ]; then
   cat >> aws_init.sh << DELIM
 # t1.micro's don't come with any swap; let's add 1G
 ## to do -- add test for micro
@@ -144,23 +157,38 @@ DELIM
 # back to the local machine
 
 gzip aws_init.sh
+base64 aws_init.sh.gz > aws_init.sh.gz.base64
 # clean up
 rm as_role_account.sh 
 
 # http://docs.amazonwebservices.com/AWSEC2/latest/CommandLineReference/ApiReference-cmd-RunInstances.html
 # "You must have the key pair where you run your script." -- https://forums.aws.amazon.com/message.jspa?messageID=88003
 # ec2-run-instances $AMI_EBS            \
-instance=`aws --region $EC2_REGION ec2 run-instances \
-     --min-count 1                    \
-     --image-id $AMI_EBS              \
-     --max-count 1                    \
-     --user-data-file aws_init.sh.gz  \
-     --key-name ec2-keypair           \
-     --monitoring \'{\"enabled\":1}\' \
-     --instance-type $EC2_SIZE        \
-     --placement \'{\"availability_zone\": \""$ZONE"\"}\' | jq '.Instances[0] | .InstanceId' -r`
+command="aws --region $EC2_REGION ec2 run-instances 
+     --min-count 1                                   
+     --image-id $AMI_EBS                             
+     --max-count 1                                   
+     --user-data file:aws_init.sh.gz.base64                 
+     --key-name ec2-keypair                          
+     --monitoring file:monitoring.json
+     --instance-type $EC2_SIZE                       
+     --placement file:placement.json"
 
-echo $instance
+echo "ec2 launch command"
+
+instance=`$command | jq '.Instances[0] | .InstanceId' -r`
+
+hostname=`aws ec2 describe-instances --instance-ids $instance | jq ' .Reservations[0] | .Instances[0] | .PublicDnsName'` 
+echo "instance started, waiting for hostname"
+while [ "$hostname" == 'null' ]
+  do
+  sleep 15
+  echo "."
+  hostname=`aws ec2 describe-instances --instance-ids $instance | jq ' .Reservations[0] | .Instances[0] | .PublicDnsName'` 
+  done
+
+echo $hostname
 
 # clean up
 rm aws_init.sh.gz
+rm aws_init.sh.gz.base64
